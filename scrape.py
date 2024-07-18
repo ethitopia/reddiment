cb021e64080f95a4452cb1dbb0740dd64222b15d
Ethan Ma <em834@cornell.edu>
2024-07-18 16:41:01 -0400

    Reddit API added

---
+++ scrape.py
@@ -0,0 +1,8 @@
+import praw 
+
+reddit = praw.Reddit(
+    client_id="jY-Juldz8QjmQjKLg2oNBg",
+    client_secret="4ZJO8wfWGG4K6KzwLpWXBqYPYntmog",
+    user_agent="test_script for subreddit scraping",
+)
+
